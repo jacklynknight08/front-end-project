be1cf62b133b5557bcb9ddf6954dcd990b295a51
Jackie <Jackie@Kyes-MacBook-Pro.local>
2020-05-07 16:38:07 -0500

    Imported typed.js to write a script for a typewriter effect

---
+++ main.js
@@ -0,0 +1,8 @@
+var typed = new Typed('.typing', {
+    strings: ["a digital marketer.", "an ad strategist.", "a copywriter."],
+    loop: true,
+    typeSpeed: 65,
+    backSpeed: 65,
+    backDelay: 1000,
+  });
+  
